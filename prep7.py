6fea102efc03fd038379f9ad254ed3952d27f009
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-09 20:51:42 -0700

    Update prep7.py

--- prep7.py
+++ prep7.py
@@ -2,15 +2,15 @@ a=input('Drink Size:\n')
 b=input('Drink type:\n')
 if a='large':
       if b='regular':
-            print('300')
+            print('300 calories')
       elif b='diet':
-            print("100")
+            print("100 calories")
       else: 
-            print('0')
+            print('0 calories')
 elif a='small':
       if b='regular':
-            print('150')
+            print('150 calories')
       elif b='diet':
-            print('50')
+            print('50 calories')
       else:
-            print('0')
+            print('0 calories')
