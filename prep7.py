de772741a97b9b3835e5185ce878131cf2472049
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-09 20:37:11 -0700

    Update prep7.py

--- prep7.py
+++ prep7.py
@@ -1,16 +1,16 @@
 a=input("Drink Size:\n")
 b=input('Drink type:\n')
-if a='large'：
-      if b='regular'：
+if a='large':
+      if b='regular':
             print('300')
-      elif b='diet'：
+      elif b='diet':
             print("100")
-      else： 
+      else: 
             print('0')
-elif a='small' ：
-      if b='regular'：
+elif a='small':
+      if b='regular':
             print('150')
-      elif b='diet'：
+      elif b='diet':
             print('50')
-      else：
+      else:
             print('0')
